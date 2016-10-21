b2cde9b0614bd46a3bfc023c428fad0df523cecc
Eyewritecode <933kss@gmail.com>
2016-10-21 23:35:58 +0900

    added corpus stopwords

--- bag_of_words.py
+++ bag_of_words.py
@@ -1,9 +1,14 @@
 import numpy as np
+from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
 count  = CountVectorizer()
+sw = stopwords.words("english")
 docs = np.array([
 	'The sun is shining',
 	'The weather is sweet',
 	'The sun is shining and the weather is sweet'])
-bag = count.fit_transform(docs)
-print count.vocabulary_
+bag = count.fit(docs)
+bag = count.transform(docs)
+print(bag)
+print(count.vocabulary_)
+print(len(sw))
